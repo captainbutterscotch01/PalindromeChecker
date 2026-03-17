cbac5ab1f1e4c58843f7c56c64649f1611f19c16
captainbutterscotch01 <labhinav06@gmail.com>
2026-03-17 14:44:40 +0530

    UC12

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,83 @@
-class PalindromeTwoPointer {
+import java.util.*;
+
+// Strategy Interface
+interface PalindromeStrategy {
+    boolean check(String str);
+}
+
+// Stack Strategy
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean check(String str) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// Deque Strategy
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean check(String str) {
+
+        Deque<Character> deque = new LinkedList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            deque.addLast(str.charAt(i));
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// Context Class
+class PalindromeContext {
+
+    private PalindromeStrategy strategy;
+
+    public PalindromeContext(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean execute(String str) {
+        return strategy.check(str);
+    }
+}
+
+// Main Class
+public class PalindromeApp {
 
     public static void main(String[] args) {
 
-        // Original string
-        String str = "radar";
+        String str = "madam";
 
-        // Convert to character array
-        char[] arr = str.toCharArray();
+        // Choose strategy dynamically
+        PalindromeStrategy strategy;
 
-        int start = 0;
-        int end = arr.length - 1;
-        boolean isPalindrome = true;
+        // Switch here
+        strategy = new DequeStrategy();
+        // strategy = new StackStrategy();
 
-        // Two-pointer comparison
-        while (start < end) {
-            if (arr[start] != arr[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
-        }
+        PalindromeContext context = new PalindromeContext(strategy);
 
-        // Display result
-        if (isPalindrome) {
+        if (context.execute(str)) {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not Palindrome");
