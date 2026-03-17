8bbb1bbf5fddb73d847d55f3a2c32c7e62e4c0e1
captainbutterscotch01 <labhinav06@gmail.com>
2026-03-17 14:15:11 +0530

    UC4

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+class PalindromeTwoPointer {
 
+    public static void main(String[] args) {
 
+        // Original string
+        String str = "radar";
+
+        // Convert to character array
+        char[] arr = str.toCharArray();
+
+        int start = 0;
+        int end = arr.length - 1;
+        boolean isPalindrome = true;
+
+        // Two-pointer comparison
+        while (start < end) {
+            if (arr[start] != arr[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
 }
